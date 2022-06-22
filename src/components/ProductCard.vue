8eff12cafe08a14df3a08dec2cd513bdf4cbdb8c
Roduan Kareem Aldeen <roduan98@gmail.com>
2022-06-22 18:21:00 +0300

    extract product card component from products section

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image">
+        <img v-bind:src="item.image" v-bind:alt="item.title">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4">{{ setLength(item.title, 50) }}</p>
+          <p class="subtitle is-6">{{ item.price }}$</p>
+        </div>
+      </div>
+
+      <div class="content">
+        {{ setLength(item.description, 80) }}
+        <br>
+        <a href="#">#{{ item.category }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ['item'],
+  props: {
+    item: {
+      type: Object
+    }
+  },
+
+  methods: {
+    setLength (str, length) {
+      if (str.length > length) {
+        return str.slice(0, length) + '...'
+      }
+      return str
+    }
+  }
+}
+</script>
--- src/components/ProductsSection.vue
+++ src/components/ProductsSection.vue
@@ -2,28 +2,12 @@
   <section class="section">
     <div class="container">
       <div class="columns is-multiline">
-          <div class="column is-12 is-4-desktop is-3-fullhd" v-for="product in products" v-bind:key="product.id">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image">
-                  <img v-bind:src="product.image" v-bind:alt="product.title">
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-content">
-                    <p class="title is-4">{{ setLength(product.title, 50) }}</p>
-                    <p class="subtitle is-6">{{ product.price }}$</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  {{ setLength(product.description, 80) }}
-                  <br>
-                  <a href="#">#{{ product.category }}</a>
-                </div>
-              </div>
-            </div>
+          <div
+            v-for="product in products"
+            v-bind:key="product.id"
+            class="column is-12 is-4-desktop is-3-fullhd"
+          >
+            <product-card :item="product" />
           </div>
         </div>
     </div>
@@ -32,8 +16,13 @@
 
 <script>
 import axios from 'axios'
+import ProductCard from './ProductCard.vue'
 
 export default {
+  components: {
+    ProductCard
+  },
+
   data () {
     return {
       products: []
@@ -44,15 +33,6 @@ export default {
     axios.get('https://fakestoreapi.com/products/').then(res => {
       this.products = res.data
     })
-  },
-
-  methods: {
-    setLength (str, length) {
-      if (str.length > length) {
-        return str.slice(0, length) + '...'
-      }
-      return str
-    }
   }
 }
 </script>
